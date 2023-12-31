7add817a4093e4f4acec2f5e35c1e5011200f993
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2023-12-31 17:05:31 +0800

    chore: abstract examples out of get_fewshot_example_selector()

--- chatbot-gpt-py/app/retriever/example_selector.py
+++ chatbot-gpt-py/app/retriever/example_selector.py
@@ -2,17 +2,15 @@ from langchain.embeddings import VertexAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.prompts import SemanticSimilarityExampleSelector
 
-from app.prompt_templates.fewshot_templates import fewshot_examples
-
-def get_fewshot_example_selector():
+def get_fewshot_example_selector(examples, k=2):
     """
     This method returns an example selector that select from a series of 
     examples to dynamically place in-context information into your prompt.
     """
     embeddings = VertexAIEmbeddings(model_name="textembedding-gecko@001")
     return SemanticSimilarityExampleSelector.from_examples(
-        fewshot_examples,
+        examples,
         embeddings,
         FAISS,
-        k=2
+        k
     )
