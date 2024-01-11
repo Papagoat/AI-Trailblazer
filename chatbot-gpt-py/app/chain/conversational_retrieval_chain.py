aab330a8bb417abe9fe6ac1fb5440929c2c18ba0
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2024-01-11 17:22:59 +0800

    chore: abstract singleton cls into separate file

--- chatbot-gpt-py/app/chain/conversational_retrieval_chain.py
+++ chatbot-gpt-py/app/chain/conversational_retrieval_chain.py
@@ -1,10 +1,9 @@
 from operator import itemgetter
-from threading import Lock
 
 from langchain.chat_models.vertexai import ChatVertexAI
 from langchain.prompts import ChatPromptTemplate, PromptTemplate, FewShotChatMessagePromptTemplate, ChatMessagePromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableSequence
 
 from app.retriever.retriever import get_vector_search_retriever, get_memory_retriever
 from app.retriever.example_selector import get_fewshot_example_selector
@@ -14,42 +13,7 @@ from app.prompt_templates.system_template import SYSTEM_TEMPLATE
 from app.prompt_templates.fewshot_templates import FEWSHOT_ANSWER_EXAMPLES, FEWSHOT_STANDALONE_QUESTION_EXAMPLES
 
 
-class SingletonMeta(type):
-    """
-    This is a thread-safe implementation of Singleton.
-    """
-
-    _instances = {}
-
-    _lock: Lock = Lock()
-    """
-    We now have a lock object that will be used to synchronize threads during
-    first access to the Singleton.
-    """
-
-    def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        # Now, imagine that the program has just been launched. Since there's no
-        # Singleton instance yet, multiple threads can simultaneously pass the
-        # previous conditional and reach this point almost at the same time. The
-        # first of them will acquire lock and will proceed further, while the
-        # rest will wait here.
-        with cls._lock:
-            # The first thread to acquire the lock, reaches this conditional,
-            # goes inside and creates the Singleton instance. Once it leaves the
-            # lock block, a thread that might have been waiting for the lock
-            # release may then enter this section. But since the Singleton field
-            # is already initialized, the thread won't create a new object.
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class ConversationalRetrievalChain(metaclass=SingletonMeta):
+class ConversationalRetrievalChain():
     """
     This class creates a chain that attempts to FIRST answer user question on the dataset before falling back on its own knowledge.
 
@@ -57,6 +21,7 @@ class ConversationalRetrievalChain(metaclass=SingletonMeta):
     """
 
     def __init__(self) -> None:
+        """This method instantiates an instance of ConversationalRetrievalChain"""
         # https://cloud.google.com/vertex-ai/docs/generative-ai/learn/models
         # pylint: disable-next=not-callable
         self.model = ChatVertexAI(
@@ -65,7 +30,7 @@ class ConversationalRetrievalChain(metaclass=SingletonMeta):
         self.retriever = get_vector_search_retriever()
         self.chain = self.get_chain()
 
-    def get_chain(self):
+    def get_chain(self) -> RunnableSequence:
         """This method instantiates the chain"""
         loaded_memory = RunnableParallel({
             "question": lambda x: x["question"],
@@ -138,13 +103,13 @@ class ConversationalRetrievalChain(metaclass=SingletonMeta):
 
         updateMemory = RunnableParallel({
             "answer": lambda x: x["answer"].content,
-            "_": lambda x: self.save_to_memory(x["question"], x["answer"].content)
+            "_": lambda x: self.save_to_memory(x["question"], x["answer"].content),
         })
 
         final_chain = loaded_memory | standalone_question | retrieved_documents | answer | updateMemory
 
         return final_chain
 
-    def save_to_memory(self, question: str, answer: str):
+    def save_to_memory(self, question: str, answer: str) -> None:
         """This method saves chat history to memory"""
         self.memory.save_context({"human": question}, {"ai": answer})
