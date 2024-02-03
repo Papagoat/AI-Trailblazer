be17480c17eed14bf548e1823b4a377ee82fbcdb
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2024-02-03 12:54:57 +0800

    feat: use BrowserRouter + update index.css

--- chatbot-gpt-frontend/src/index.tsx
+++ chatbot-gpt-frontend/src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./components/App/App";
+import "./index.css"
 
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
