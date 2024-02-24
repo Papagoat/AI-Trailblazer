0bef395394e464a4ccb55c039cfa41cacd506f47
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2024-02-24 21:42:13 +0800

    chore: abstracted initial daisy logo page to component

---
+++ chatbot-gpt-frontend/src/components/GrantEligibilityDisplay/InitialDisplay.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./InitialDisplay.module.css";
+import { DaisyWordImage, EllipsesGroupImage } from "src/assets";
+
+export const InitialDisplay = () => (
+  <section className={styles["wrapper"]}>
+    <EllipsesGroupImage className={styles["ellipses-group"]} />
+    <div className={styles["branding"]}>
+      <section className={styles["title"]}>
+        <p>Dependable AI Support for You</p>
+        <DaisyWordImage />
+      </section>
+      <section className={styles["description"]}>
+        <p>Your Personal Guide to Government Support</p>
+        <p>Get answers on Government Schemes, just ask in your own words.</p>
+      </section>
+    </div>
+  </section>
+);
