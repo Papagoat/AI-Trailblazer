48be4eeca38d0f2fa100520c8a9c332b7ed9cf68
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2024-02-24 21:41:12 +0800

    feat(GrantEligibilityDisplay): use AppContext + add DetailsPanel/Card

--- chatbot-gpt-frontend/src/components/GrantEligibilityDisplay/GrantEligibilityDisplay.tsx
+++ chatbot-gpt-frontend/src/components/GrantEligibilityDisplay/GrantEligibilityDisplay.tsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 
 import styles from "./GrantEligibilityDisplay.module.css";
 
-import { DaisyWordImage, EllipsesGroupImage } from "src/assets";
+import { AppContext } from "src/components/AppContext/AppContext";
+import { InitialDisplay } from "./InitialDisplay";
+import { DetailCard } from "src/components/common/DetailCard/DetailCard";
+import { DetailPanel } from "src/components/common/DetailPanel/DetailPanel";
 
 interface IProps {
   className: string;
 }
 
 export const GrantEligibilityDisplay = ({ className }: IProps) => {
+  const displayContainerRef = useRef<HTMLDivElement>(null);
+  const { descriptions } = useContext(AppContext);
+
+  const [displayHeight, setDisplayHeight] = useState<number>();
+  const [details, setDetails] = useState<string[]>([]);
+
+  useEffect(() => {
+    setDetails((prevDetails) => [...prevDetails, ...descriptions]);
+  }, [descriptions]);
+
+  // TODO: Might want to move into AppContext and implement the same height
+  // for both GrantEligibilityDisplay / GenAIDisplay
+  useEffect(() => {
+    const displayContainerHeight = displayContainerRef.current?.offsetHeight;
+    setDisplayHeight(displayContainerHeight);
+    if (displayContainerRef.current) {
+      displayContainerRef.current.style.height = `${displayHeight}px`;
+    }
+  }, [displayHeight]);
+
   return (
-    <div className={`${styles["wrapper"]} ${className}`}>
-      <EllipsesGroupImage className={styles["ellipses-group"]} />
-      <div className={styles["branding"]}>
-        <section className={styles["title"]}>
-          <p>Dependable AI Support for You</p>
-          <DaisyWordImage />
-        </section>
-        <section className={styles["description"]}>
-          <p>Your Personal Guide to Government Support</p>
-          <p>Get answers on Government Schemes, just ask in your own words.</p>
-        </section>
-      </div>
+    <div
+      className={`${styles["wrapper"]} ${className}`}
+      ref={displayContainerRef}
+    >
+      <InitialDisplay />
+      {details.length > 0 && (
+        <>
+          {details.map((d, i) => (
+            <DetailPanel key={i}>
+              <DetailCard title={"Title of nested card"} content={d} />
+            </DetailPanel>
+          ))}
+        </>
+      )}
     </div>
   );
 };
