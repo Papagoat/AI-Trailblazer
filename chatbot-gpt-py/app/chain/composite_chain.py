a73999253dcd300560a39d46e5302dd94a494966
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2024-03-01 20:22:17 +0800

    feat: update chains to use new prompt templates

--- chatbot-gpt-py/app/chain/composite_chain.py
+++ chatbot-gpt-py/app/chain/composite_chain.py
@@ -12,13 +12,13 @@ from app.chain.singleton import SingletonMeta
 from app.prompt_templates.paraphrase_and_suggest_template import PARAPHRASE_AND_SUGGEST_TEMPLATE
 from app.chain.conversational_retrieval_chain import ConversationalRetrievalChain
 
-
-class Answer(BaseModel):
+class ParaphraseAndSuggestOutput(BaseModel):
     """
     Typings for paraphrased chain output
     """
     answer: str = Field(description="answer")
-    reply_options: List[str] = Field(description="list of suggested user responses")
+    reply_options: List[str] = Field(
+        description="list of suggested user responses")
 
 
 class CompositeChain(metaclass=SingletonMeta):
@@ -60,12 +60,14 @@ class CompositeChain(metaclass=SingletonMeta):
                 })
             | {
                 "response": self.paraphrase_and_suggest_chain,
-                "information": lambda x: x["conversational_chain"]["information"] if "information" in x["conversational_chain"] else []
+                "information": lambda x: x["conversational_chain"]["information"] if "information" in x["conversational_chain"] else [],
+                "topic": lambda x: x["conversational_chain"]["topic"]
             }
             | {
                 "answer": lambda x: x['response']["answer"],
                 "reply_options": lambda x: x['response']["reply_options"],
-                "information": lambda x: x["information"]
+                "information": lambda x: x["information"],
+                "topic": lambda x: x["topic"]
             }
         )
 
@@ -73,7 +75,7 @@ class CompositeChain(metaclass=SingletonMeta):
 
     def get_paraphrase_and_suggest_chain(self) -> RunnableSequence:
         """This method returns a chain that paraphrases the answer, and provides suggested human responses"""
-        parser = JsonOutputParser(pydantic_object=Answer)
+        parser = JsonOutputParser(pydantic_object=ParaphraseAndSuggestOutput)
         PARAPHRASE_AND_SUGGEST_PROMPT = PromptTemplate.from_template(PARAPHRASE_AND_SUGGEST_TEMPLATE, partial_variables={
             "format_instructions": parser.get_format_instructions()
         })
@@ -81,7 +83,8 @@ class CompositeChain(metaclass=SingletonMeta):
         paraphrase_and_suggest_chain = (
             {
                 "original_answer": lambda x: x["conversational_chain"]["answer"],
-                "eligibility_table": lambda x: self.eligibility_dict
+                "eligibility_table": lambda x: self.eligibility_dict,
+                "question": lambda x: x["conversational_chain"]["question"]
             }
             | PARAPHRASE_AND_SUGGEST_PROMPT
             | ChatVertexAI(model_name="chat-bison-32k", temperature=0, verbose=True, max_output_tokens=8192)
