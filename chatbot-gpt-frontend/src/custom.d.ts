1392f70fc57c06d4372547e41a7ec5ea9e2ff3ca
tin_kit_wong <wong_tin_kit@tech.gov.sg>
2024-03-01 20:24:41 +0800

    feat(GenAIDsiplay): add scrolling behavior + styles for nudge section

--- chatbot-gpt-frontend/src/custom.d.ts
+++ chatbot-gpt-frontend/src/custom.d.ts
@@ -7,3 +7,10 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+
+declare module '*.svg' {
+  import React = require('react')
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+}
